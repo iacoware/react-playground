739a3a613ef52b4d50b9a84a517c6cf9bfaa1201
Massimo Iacolare <info@massimoiacolare.it>
2021-08-16 14:50:58 +0200

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./main.css"
-import App from "./views/app.js"
+import App from "./app/app"
 
 console.log("Hello World!")
 
-ReactDOM.render(<App name="World" />, document.getElementById("app-container"))
+ReactDOM.render(<App />, document.getElementById("app-container"))
