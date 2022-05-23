45260a5f48f0a441c9006e997461a34d75cae3b3
Massimo Iacolare <info@massimoiacolare.it>
2022-05-23 15:29:58 +0200

    Update deps; switched to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import "./main.css"
 import App from "./app/app"
 
 console.log("Hello World!")
 
-ReactDOM.render(<App />, document.getElementById("app-container"))
+const root = createRoot(document.getElementById("app-container")!)
+root.render(<App />)
